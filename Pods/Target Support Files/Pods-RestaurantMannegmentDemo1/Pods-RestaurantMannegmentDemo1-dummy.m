b1746c701535c1369c57ffad8593772027040534
Mukesh Lokare <mukeshlokare555@gmail.com>
2017-12-05 17:27:33 +0530

    commiting first time

---
+++ Pods/Target Support Files/Pods-RestaurantMannegmentDemo1/Pods-RestaurantMannegmentDemo1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RestaurantMannegmentDemo1 : NSObject
+@end
+@implementation PodsDummy_Pods_RestaurantMannegmentDemo1
+@end
---
+++ RestaurantMannegmentDemo1/RestaurantMannegmentDetails.swift
@@ -0,0 +1,16 @@
+//
+//  RestaurantMannegmentDetails.swift
+//  RestaurantMannegmentDemo1
+//
+//  Created by Manisha Roy on 23/11/17.
+//  Copyright © 2017 Neosofttech Technologies. All rights reserved.
+//
+
+import Foundation
+struct UserInfo {
+    var userId: Int!
+    var firstName: String!
+    var lastName: String!
+    var email: String!
+    var password: String!
+}
